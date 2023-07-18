77f47eb43f5f19db25bd2cd5e5261779ab72e6be
maLouiss <tahayassinbenromdhane@gmail.com>
2023-07-18 09:16:45 +0100

    first commit

---
+++ S2 - CSS/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// alert("Hello world");
+// console.log("hello wassim!")
+
+
+// var nom = prompt("Entrez votre Nom?");
+// taper clg : console.log
+// console.log("😀");
+
+
+// document.getElementById("parag").innerHTML = "hello worlduzejgnk efk nzefk nzef";
+// console.log(p);
